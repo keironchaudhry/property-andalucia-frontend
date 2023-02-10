c79c5c9909f535845cc82f3d0a5d535348b00bc0
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-02-10 09:40:45 +0000

    Create navbar structure and import brand logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+import logo from "../assets/logo.png";
 
 function NavBar() {
   return (
-    <div>navbar placeholder</div>
-  )
+    <Navbar expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand>
+          <img src={logo} alt="Logo" height="80" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default NavBar
+export default NavBar;
