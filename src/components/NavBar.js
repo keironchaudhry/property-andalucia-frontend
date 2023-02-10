71b31d7aaff18fe564493e535f8cb551a2e57af3
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-02-10 15:39:17 +0000

    Create customised css for navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,26 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 
+import styles from "../styles/NavBar.module.css";
 import logo from "../assets/logo.png";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>
+        <Navbar.Brand className={styles.Logo}>
           <img src={logo} alt="Logo" height="80" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-right">
-            <Nav.Link>
+            <Nav.Link className={styles.NavLink}>
               <i className="fas fa-home"></i>Home
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link className={styles.NavLink}>
               <i className="fas fa-sign-in-alt"></i>Login
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link className={styles.NavLink}>
               <i className="fas fa-user-plus"></i>Sign Up
             </Nav.Link>
           </Nav>
