73fe83304980f86069b99ea538e44ce9e2bb3a4e
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-02-12 15:13:27 +0000

    Create basic login form component

--- src/pages/auth/LoginForm.js
+++ src/pages/auth/LoginForm.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+
+import { Form, Button, Col, Row, Container } from "react-bootstrap";
 
 const LoginForm = () => {
   return (
-    <h1>LoginForm</h1>
-  )
-}
+    <Row className={styles.Row}>
+      <Col className="my-auto mx-auto py-2 p-md-2" md={8} lg={6}>
+        <Container className={`mt-5 ${appStyles.Content}`}>
+          <h1 className={styles.Header}>Login</h1>
 
-export default LoginForm
+          <Container className={`mt-3 mb-3 ${appStyles.Content}`}>
+            <Link className={styles.Link} to="/signin">
+              New to Property Andalucia? <span>Create your account here.</span>
+            </Link>
+          </Container>
+
+          <Form>
+            <Form.Group className="mb-3" controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="text"
+                placeholder="Username"
+                name="username"
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="password1">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="Password"
+                name="password"
+              />
+            </Form.Group>
+
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+              type="submit"
+            >
+              Sign Up
+            </Button>
+          </Form>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
+
+export default LoginForm;
