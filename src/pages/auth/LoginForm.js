7b2b733903d802269b6a45310d932d299df50713
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-02-12 19:06:39 +0000

    Create handle change function to handle user login data

--- src/pages/auth/LoginForm.js
+++ src/pages/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
@@ -8,6 +8,20 @@ import appStyles from "../../App.module.css";
 import { Form, Button, Col, Row, Container } from "react-bootstrap";
 
 const LoginForm = () => {
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const { username, password } = signInData;
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <Row className={styles.Row}>
       <Col className="my-auto mx-auto py-2 p-md-2" md={8} lg={6}>
@@ -15,7 +29,7 @@ const LoginForm = () => {
           <h1 className={styles.Header}>Login</h1>
 
           <Container className={`mt-3 mb-3 ${appStyles.Content}`}>
-            <Link className={styles.Link} to="/signin">
+            <Link className={styles.Link} to="/signup">
               New to Property Andalucia? <span>Create your account here.</span>
             </Link>
           </Container>
@@ -28,6 +42,8 @@ const LoginForm = () => {
                 type="text"
                 placeholder="Username"
                 name="username"
+                value={username}
+                onChange={handleChange}
               />
             </Form.Group>
 
@@ -38,11 +54,13 @@ const LoginForm = () => {
                 type="password"
                 placeholder="Password"
                 name="password"
+                value={password}
+                onChange={handleChange}
               />
             </Form.Group>
 
             <Button
-              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright} mt-3`}
               type="submit"
             >
               Sign Up
