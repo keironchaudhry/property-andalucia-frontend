6e4301367f0d857f9483c8b6cc9912b90e7ca091
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-02-19 13:31:28 +0000

    Add routes for feed and savedpaths

--- src/App.js
+++ src/App.js
@@ -9,14 +9,44 @@ import LoginForm from "./pages/auth/LoginForm";
 import PropertyCreateForm from "./pages/property/PropertyCreateForm";
 import PropertyPage from "./pages/property/PropertyPage";
 import PropertyList from "./pages/property/PropertyList";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <PropertyList message="No results found."/>} />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <PropertyList message="No results found, please try adjusting your province filter." />
+            )}
+          />
+          <Route
+            exact
+            path="/feed"
+            render={() => (
+              <PropertyList
+                message="No results found, you will find properties from sellers you follow here."
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/saved"
+            render={() => (
+              <PropertyList
+                message="No results found, any properties you save will be listed here."
+                filter={`saves__owner__profile=${profile_id}&ordering=-saves__created_at&`}
+              />
+            )}
+          />
           <Route exact path="/login" render={() => <LoginForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route
@@ -24,11 +54,7 @@ function App() {
             path="/property/create"
             render={() => <PropertyCreateForm />}
           />
-          <Route
-            exact
-            path="/property/:id"
-            render={() => <PropertyPage />}
-          />
+          <Route exact path="/property/:id" render={() => <PropertyPage />} />
           <Route render={() => <p>Page not found.</p>} />
         </Switch>
       </Container>
