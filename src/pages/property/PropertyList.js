040f5c771ed548ab48d006e8201227f31853d531
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-02-23 09:03:27 +0000

    Create additional search filter forms

--- src/pages/property/PropertyList.js
+++ src/pages/property/PropertyList.js
@@ -62,7 +62,7 @@ function PropertyList({ message, filter = "" }) {
 
   return (
     <Row className="mt-5">
-      <Col className={`${styles.ColContainer} py-2 p-0 p-lg-2`} lg={8}>
+      <Col className={`${formStyles.ColContainer} py-2 p-0 p-lg-2`} lg={8}>
         {/* <p>Popular profiles mobile</p> */}
         {hasLoaded ? (
           <>
@@ -96,15 +96,17 @@ function PropertyList({ message, filter = "" }) {
         {/* Province Search Feed */}
         <div className={formStyles.FormContainer}>
           <Form>
-            <Form.Group className="text-center" controlId="province">
-              <Form.Label className="h5 mb-3">Search by province</Form.Label>
+            <Form.Group className="text-center mt-2" controlId="province">
+              <Form.Label>Search by province</Form.Label>
               <Form.Control
                 as="select"
                 type="text"
                 name="province"
                 value={province}
                 onChange={(event) => setProvince(event.target.value)}
+                className="text-center"
               >
+                <option value="">Select a province</option>
                 <option value="huelva">Huelva</option>
                 <option value="sevilla">Sevilla</option>
                 <option value="cadiz">Cadiz</option>
@@ -116,8 +118,75 @@ function PropertyList({ message, filter = "" }) {
               </Form.Control>
             </Form.Group>
 
+            <Form.Group className="text-center" controlId="propertyType">
+              <Form.Label>Search by property</Form.Label>
+              <Form.Control
+                as="select"
+                type="text"
+                className="text-center"
+                name="propertyType"
+              >
+                <option value="">Select a property type</option>
+                <option value="apartment">Apartment</option>
+                <option value="flat">Flat</option>
+                <option value="townhouse">Townhouse</option>
+                <option value="villa">Villa</option>
+                <option value="residential housing estate">Residential Housing Estate</option>
+                <option value="country property">Country Property</option>
+                <option value="bungalow">Bungalow</option>
+              </Form.Control>
+            </Form.Group>
+
+            <div className="d-flex gap-3">
+              <Form.Group className="text-center mr-2" controlId="minPrice">
+                <Form.Label>Min Price</Form.Label>
+                <Form.Control
+                  className="text-center"
+                  type="number"
+                  name="minPrice"
+                  min={0}
+                  step={50000}
+                />
+              </Form.Group>
+
+              <Form.Group className="text-center ml-2" controlId="maxPrice">
+                <Form.Label>Max Price</Form.Label>
+                <Form.Control
+                  className="text-center"
+                  type="number"
+                  name="maxPrice"
+                  min={0}
+                  step={50000}
+                />
+              </Form.Group>
+            </div>
+
+            <div className="d-flex gap-3">
+              <Form.Group className="text-center mr-2" controlId="minBedrooms">
+                <Form.Label className="me-2">Min Bedrooms</Form.Label>
+                <Form.Control
+                  className="text-center"
+                  type="number"
+                  name="minBedrooms"
+                  min={0}
+                  step={1}
+                />
+              </Form.Group>
+
+              <Form.Group className="text-center ml-2" controlId="maxBedrooms">
+                <Form.Label className="me-2">Max Bedrooms</Form.Label>
+                <Form.Control
+                  className="text-center"
+                  type="number"
+                  name="maxBedrooms"
+                  min={0}
+                  step={1}
+                />
+              </Form.Group>
+            </div>
+
             {/* Buttons */}
-            <div className="d-flex flex-row justify-content-between">
+            <div className="flex-row text-center pt-1 pb-1">
               <Button
                 className={`${btnStyles.Button} ${btnStyles.Bright}`}
                 type="submit"
@@ -125,12 +194,6 @@ function PropertyList({ message, filter = "" }) {
                 onClick={clearSearch}
               >
                 <i class="fa-solid fa-rotate-left"></i>Clear Filter
-              </Button>
-              <Button
-                className={`${btnStyles.Button} ${btnStyles.Bright}`}
-                type="submit"
-              >
-                <i class="fa-solid fa-filter"></i>More Filters
               </Button>
             </div>
           </Form>
