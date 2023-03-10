0987f1bfe2d126b06bbac1bed985b45d37c51724
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-03-10 17:49:42 +0000

    Create and import notfound component to main app

---
+++ src/components/NotFound.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NoResults from "../assets/noresults.png";
+import styles from "../styles/NotFound.module.css";
+import Asset from "./Asset";
+
+
+const NotFound = () => {
+  return (
+    <div className={styles.NotFound}>
+        <Asset src={NoResults} message={"The page you're looking for does not exist."} />
+    </div>
+  )
+}
+
+export default NotFound
