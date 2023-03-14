c8e8756326f0ef7a35338ea8060ece20f15724e0
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-03-14 17:34:32 +0000

    Create blog creation route

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import UsernameForm from "./pages/profiles/UsernameForm";
 import UserPasswordForm from "./pages/profiles/UserPasswordForm";
 import ProfileEditForm from "./pages/profiles/ProfileEditForm";
 import NotFound from "./components/NotFound";
+import BlogCreateForm from "./pages/blog/BlogCreateForm";
 
 /**
  * Code adapted from Code Institute's "Moments" walkthrough.
@@ -85,6 +86,11 @@ function App() {
             exact
             path="/profiles/:id/edit"
             render={() => <ProfileEditForm />}
+          />
+          <Route
+            exact
+            path="/blog/create"
+            render={() => <BlogCreateForm />}
           />
           <Route render={() => <NotFound />} />
         </Switch>
