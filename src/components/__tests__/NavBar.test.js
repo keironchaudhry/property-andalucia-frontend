23916703fd8dac793b2093ae850803fc181b2598
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-05-06 13:26:58 +0000

    Testing: Test the existence of logged out navbar links

--- src/components/__tests__/NavBar.test.js
+++ src/components/__tests__/NavBar.test.js
@@ -2,6 +2,18 @@ import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from "../NavBar";
 
+test("Brand logo renders on Navbar component", () => {
+  render(
+    <Router>
+      <NavBar />
+    </Router>
+  );
+
+  const brandLogo = screen.getByAltText("Logo");
+
+  expect(brandLogo).toBeInTheDocument();
+});
+
 test('Renders the "login" link on Navbar component', () => {
   render(
     <Router>
@@ -12,4 +24,28 @@ test('Renders the "login" link on Navbar component', () => {
   const loginLink = screen.getByText("Login");
 
   expect(loginLink).toBeInTheDocument();
+});
+
+test('Renders the "sign up" link', () => {
+  render(
+    <Router>
+      <NavBar />
+    </Router>
+  );
+
+  const signUpLink = screen.getByText("Sign Up");
+
+  expect(signUpLink).toBeInTheDocument();
+});
+
+test('Renders the "Our Blog" link', () => {
+  render(
+    <Router>
+      <NavBar />
+    </Router>
+  );
+
+  const ourBlogLink = screen.getByText("Our Blog");
+
+  expect(ourBlogLink).toBeInTheDocument();
 });
