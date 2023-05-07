802a548c46c540bbabfac8ec41ef9a80add88131
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-05-07 13:42:35 +0200

    Testing: Create test for avatar component file

---
+++ src/components/__tests__/Avatar.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import Avatar from "../Avatar";
+
+test("Avatar image renders when source prop is true", () => {
+  render(<Avatar src={true} />);
+
+  const avatarImg = screen.getByAltText("Avatar");
+
+  expect(avatarImg).toBeInTheDocument();
+});
